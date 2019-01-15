71cafb8f068544515560eca3903ad9434316e09b
Charles Harries <charles@crtr.co.uk>
2019-01-15 12:02:48 +0000

    Made a change

--- woostock/woostock.php
+++ woostock/woostock.php
@@ -10,6 +10,8 @@ Author URI: http://www.woostock.com
 
 */
 
+// Here's a change
+
 // Check if WooCommerce is active
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	
